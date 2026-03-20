bbbef36f270cd7ebb015e2b26016d0a88bff6dfd
Anatoly <pro1chuh@gmail.com>
2026-03-20 12:12:22 +0300

    Initial project structure

---
+++ main.py
@@ -0,0 +1,24 @@
+import argparse
+import sys
+
+from gaica_bot.client import BotClient
+from gaica_bot.controller import BotController
+
+
+def parse_args() -> argparse.Namespace:
+    parser = argparse.ArgumentParser()
+    parser.add_argument("host")
+    parser.add_argument("port", type=int)
+    return parser.parse_args()
+
+
+def main() -> int:
+    args = parse_args()
+    controller = BotController()
+    client = BotClient(args.host, args.port, controller)
+    client.run()
+    return 0
+
+
+if __name__ == "__main__":
+    raise SystemExit(main())
