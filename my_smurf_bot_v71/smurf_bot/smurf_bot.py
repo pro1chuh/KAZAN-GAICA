bf8439e88e4c6d367dcb7750cb294b75822f6a30
erorms <dgodovets@gmail.com>
2026-03-23 19:49:48 +0500

    Update AdvancedBot class with optimized parameters

--- my_smurf_bot_v71/smurf_bot/smurf_bot.py
+++ my_smurf_bot_v71/smurf_bot/smurf_bot.py
@@ -1,35 +1,14 @@
-# Updated Bot Logic
+class AdvancedBot:
+    REVOLVER_RANGE = 320.0
+    UZI_RANGE = 260.0
+    BULLET_DANGER_DIST = 140.0
 
-# Constants for weapon ranges
-REVOLVER_RANGE = 320  # Increased from 240
-UZI_RANGE = 260  # Increased from 180
+    def __init__(self):
+        self.aim_lead = 4  # Number of iterations for aiming lead
+        self.stuck_timer_threshold = 8  # Threshold for stuck timer
+        self.uzi_shoot_threshold_dot = 0.45  # UZI shoot threshold dot
+        self.strafe_interval = 18  # Interval for strafing
+        self.enemy_speed_tracking_smoothing = 0.5  # Smoothing factor for enemy speed tracking
+        self.bullet_dodge_future_ticks = 6  # Future ticks for bullet dodge
 
-# Bullet dodge settings
-bullet_dodge_future_ticks = 6  # Increased from 4
-
-# Aim lead settings
-aim_lead_iterations = 4  # Increased from 2
-
-# Stuck timer settings
-stuck_timer_threshold = 8  # Reduced from 15
-
-# Bullet danger distance
-BULLET_DANGER_DIST = 140  # Increased from 90
-
-# UZI shooting threshold
-UZI_shooting_dot_threshold = 0.45  # Reduced from 0.60
-
-# HUNT strategy
-HUNT_aggressiveness = 0.9  # Increased to 0.9
-
-# DUEL distance management improvements
-# (Add code that manages distance during duels here)
-
-# Predictive movement based on enemy velocity
-# (Add code for predictive movement here)
-
-# Enemy speed tracking smoothing
-enemy_speed_tracking_smoothing = 0.5  # Improved from 0.4
-
-# Strafe interval optimization
-strafe_interval = 18  # Optimized from 22 ticks
+    # Additional methods and functionality for AdvancedBot can be added here
